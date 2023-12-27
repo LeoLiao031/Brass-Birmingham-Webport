539d617644cb89df81496b35a9c85638d842db18
Oscar Yang <oscaryangb@gmail.com>
2023-12-27 16:11:08 -0500

    Input validator stuff

--- Frontend/not-dominion/src/app/gameplay_module/Input/InputValidator.ts
+++ Frontend/not-dominion/src/app/gameplay_module/Input/InputValidator.ts
@@ -1,5 +1,5 @@
-import { Input, Data, InputType} from "./Input"
-import { PublicState, LocalState, Card } from "../State/GameState";
+import { Input, Data, InputType, DataType} from "./Input"
+import { PublicState, LocalState, Card, CardType } from "../State/GameState";
 
 function ValidateInput(input : Input,
                        publicState : PublicState, 
@@ -24,13 +24,94 @@ function ValidateInput(input : Input,
             return ValidateLoan(publicState, input);
         case InputType.Scout:
             return ValidateScout(publicState, input);
+        default:
+            return false;
     }
-
-    return true;
 }
 
 function ValidateBuild(publicState : PublicState, input : Input) : boolean
 {
+    let industryIndex : number = -1;
+    let townIndex : number = -1;
+    let tileIndex : number = -1;
+
+    // Read input //////////////////////////////////////////////////////
+    for (let i = 0; i < input.data.length; i++) 
+    {
+        switch (input.data[i].dataType) 
+        {
+            case DataType.Industry:
+                if (input.data.length >= 1)
+                {
+                    industryIndex = input.data[i].stack[0];
+                }
+                break;
+            case DataType.Tile:
+                if (input.data.length >= 2)
+                {
+                    townIndex = input.data[i].stack[0];
+                    tileIndex = input.data[i].stack[1];
+                }
+                break;
+        }
+    }
+
+    if (industryIndex < 0 || townIndex < 0 || tileIndex < 0)
+    {
+        return false;
+    }
+
+    // Check for appropriate card ///////////////////////////////
+    if (input.cardUsed.type == CardType.Location && !input.cardUsed.isWild)
+    {
+        let canBuildInLocation : boolean = false;
+        for (let i : number = 0; i < input.cardUsed.indexes.length; i++)
+        {
+            if (input.cardUsed.indexes[i] == townIndex)
+            {
+                canBuildInLocation = true;
+                break;
+            }
+        }
+
+        if (!canBuildInLocation)
+        {
+            return false;
+        }
+    }
+    else if (input.cardUsed.type == CardType.Industry)
+    {
+        if (!input.cardUsed.isWild)
+        {
+            let canBuildIndustry : boolean = false;
+            for (let i : number = 0; i < input.cardUsed.indexes.length; i++)
+            {
+                if (input.cardUsed.indexes[i] == industryIndex)
+                {
+                    canBuildIndustry = true;
+                    break;
+                }
+            }
+    
+            if (!canBuildIndustry)
+            {
+                return false;
+            }
+        }
+
+        // Check if in network 
+    }
+
+    // Check if tile is empty
+
+    // Check if tile can hold building
+    
+    // Check for enough money
+
+    // Check for enough coal
+
+    // Check for enough iron
+
     return true;
 }
 
--- Frontend/not-dominion/src/app/gameplay_module/State/GameState.ts
+++ Frontend/not-dominion/src/app/gameplay_module/State/GameState.ts
@@ -152,7 +152,7 @@ export class PublicState
     }
 }
 
-enum CardType
+export enum CardType
 {
     Location,
     Industry
