1060dc93161c597fe64f17f55f68c361bbbdb574
LeoLiao031 <114187109+LeoLiao031@users.noreply.github.com>
2024-02-15 10:29:20 -0500

    added unplaced industry component and constants file

--- Frontend/not-dominion/src/app/page.tsx
+++ Frontend/not-dominion/src/app/page.tsx
@@ -1,10 +1,7 @@
 import { StartGame } from './gameplay_module/State/Game'
-import IndustryTile from './components/IndustryTile'
 
 export default function Home() {
   const gameData = StartGame(4);
-  const red = "#6e0e08"
-  const grey = "#302c2c"
   return (
     <>
     </>
